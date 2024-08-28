e1b6bcfc0ff6b7adc93a2a555244c5c4ef23f881
Uttam Kumar <uttammaurya377@gmail.com>
2024-08-28 10:01:08 +0530

    important

---
+++ vovel.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * vovel
+ */
+public class vovel {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a word");
+        String input = scanner.nextLine();
+        input = input.toLowerCase();
+        int count =0;
+        for (int i = 0; i < input.length(); i++) {
+            char ch =input.charAt(i);
+            if (ch == 'a'||ch == 'e'||ch == 'i'||ch == 'o'||ch == 'u') {
+                count++;
+                
+            }
+            
+        }
+        System.out.println("the output is"+count);
+    }
+}
