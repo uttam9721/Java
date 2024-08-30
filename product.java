75eea7886cd2dc8c671237cad8d0625db29c2c79
Uttam Kumar <uttammaurya377@gmail.com>
2024-08-31 02:25:48 +0530

    callvalue

--- product.java
+++ product.java
@@ -1,12 +1,16 @@
 public class product {
-    public static void mul(int a,int b){
+    public static int mul(int a,int b){
         int mul=a*b;
-        System.out.println("Multiplication of "+a+" and "+b+" is "+mul);
+        // System.out.println("Multiplication of "+a+" and "+b+" is "+mul);
+        return mul;
     }
     public static void main(String[] args) {
-        int a = 5;
-        int b = 10;
-        mul(a,b);//in call by value always pass copy value it does not pass original value
+        // int a = 5;
+        // int b = 10;
+        int prod=  mul(10,20);
+    //in call by value always pass copy value it does not pass original value
+    // }
+    System.out.println(prod);
     }
     
 }
