372c8f527118b1d8be269786eada68be34b1cb56
Uttam Kumar <uttammaurya377@gmail.com>
2024-08-31 02:14:50 +0530

    mul

---
+++ product.java
@@ -0,0 +1,12 @@
+public class product {
+    public static void mul(int a,int b){
+        int mul=a*b;
+        System.out.println("Multiplication of "+a+" and "+b+" is "+mul);
+    }
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        mul(a,b);
+    }
+    
+}
