3316ec3621a120dcfa1252bbb4bf9907403ed02b
Uttam Kumar <uttammaurya377@gmail.com>
2024-08-31 01:38:52 +0530

    function

--- function.java
+++ function.java
@@ -1,8 +1,26 @@
 public class function {
 
 
+    public static void printHello(){
+        System.out.println("Hello");
+        // System.out.println("Hello");
+        // System.out.println("Hello");
+        // System.out.println("Hello");
+    }
+
     // this is main function void returns always null value
     public static void main(String[] args) {
+        // System.out.println("");
+        printHello();
+        printHello();
+        printHello();
+        printHello();
         
     }
 }
+
+// structure of function
+// return type name (){
+//body
+// return statement
+// }
