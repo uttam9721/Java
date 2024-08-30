ef3fed681649f6419c03e574996c53d37f1bff00
Uttam Kumar <uttammaurya377@gmail.com>
2024-08-31 01:55:12 +0530

    paramfunction

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void functionwithpara(int num1, int num2) {
+        int sum = num1 + num2;
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        functionwithpara(a, b);
+    }
+}
