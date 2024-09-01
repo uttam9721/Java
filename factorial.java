aeea448be0d931b8445d70cb900d1d6a9322d038
Uttam Kumar <uttammaurya377@gmail.com>
2024-09-02 00:10:23 +0530

    fac

--- factorial.java
+++ factorial.java
@@ -1,3 +1,17 @@
-public class factorial {
-    
+class factorial {
+
+    public static int factt(int n){
+        int f=1;
+        for (int i = 0; i < n; i++) {
+            f=f*i;
+            
+        }
+        return f;
+    }
+    // System.out.println(")  
+    }
+    public static void main(String[] args) {
+        
+     System.out.println(factt(4));
+        
 }
