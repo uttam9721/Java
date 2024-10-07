74ad07ed788eaaadadbce233cf09cf716e69c20f
Uttam Kumar <uttammaurya377@gmail.com>
2024-10-07 11:39:54 +0530

    array

---
+++ Recursion/IncNo.java
@@ -0,0 +1,11 @@
+package Recursion;
+
+public class IncNo {
+    public static void IncNumber(int n){
+        
+    }
+    public static void main(String[] args) {
+        int n = 5;
+    }
+    
+}
