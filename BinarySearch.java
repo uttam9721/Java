3356b975383b14d9d5921ea3b3b33bf8ec259da3
Uttam Kumar <uttammaurya377@gmail.com>
2024-10-15 02:18:51 +0530

    pairSum

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,27 +1,28 @@
+/**
+ * BinarySearch
+ */
 public class BinarySearch {
-    public static int binarySearch(int numbers[], int key) {
-        int start =0;
-        int end = numbers.length - 1;
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            // comparisons
-            if (numbers[mid] == key) {
-                return mid;
-                }
-            if(numbers[mid]<key){
-                start=mid+1;
+    static int binarySearch(int numbers[],int key ){
+        int start = 0,end = numbers.length-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            // comparision
+            if (numbers[mid]==key) {
+                return mid;   
             }
-            else{
-                end=mid-1;
-                }
-                }
-                return -1;
+            if (numbers[mid]<key) {
+                start=mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return -1;
     }
+
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
-        int key =4;
-    
-    System.out.println(binarySearch(numbers,key));
-} 
-    
+        int numbers[]={2,4,6,8,10,12,14};
+        int key = 10;
+        System.out.println(binarySearch(numbers, key));
+        
+    }
 }
