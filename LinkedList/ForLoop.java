7001dd6387f9a7aec9cfaf9e4540c8464fe00ba1
Uttam Kumar <uttammaurya377@gmail.com>
2024-11-01 20:07:44 +0530

    ll

--- LinkedList/ForLoop.java
+++ LinkedList/ForLoop.java
@@ -21,11 +21,12 @@
      Node c = new Node(7);
      Node d = new Node(8);
      Node e = new Node(800);
-     Node e = new Node(900);
+     Node f = new Node(900);
      a.next=b;
         b.next=c;
         c.next=d;
         d.next=e;
+        e.next=f;
     //  System.out.println(a.data);
     // Node temp = a;
     // while (temp != null) {
