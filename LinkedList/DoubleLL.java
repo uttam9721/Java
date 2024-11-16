8c6e9476cc588695e769671cafc99e93e5a2af64
Uttam Kumar <uttammaurya377@gmail.com>
2024-11-16 20:55:17 +0530

    stack

---
+++ LinkedList/DoubleLL.java
@@ -0,0 +1,30 @@
+package LinkedList;
+
+public class DoubleLL {
+    public class Node{
+        int data;
+        Node next;
+        Node  prev;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    
+    // add
+
+
+    // remove
+    public static void main(String[] args) {
+        DoubleLL dll = new DoubleLL();
+        
+    }
+    
+}
---
+++ Stack/Basic.java
@@ -0,0 +1,8 @@
+package Stack;
+
+public class Basic {
+
+    public static void main(String[] args) {
+        
+    }
+}
