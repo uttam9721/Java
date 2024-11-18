d6f6ef7949b8758849ce7b53b627e81ee58d0ea4
Uttam Kumar <uttammaurya377@gmail.com>
2024-11-18 13:52:38 +0530

    stack

--- Stack/Basic.java
+++ Stack/Basic.java
@@ -1,8 +1,18 @@
 package Stack;
-
+import java.util.*;
 public class Basic {
 
     public static void main(String[] args) {
-        
+        Stack<Integer> st = new Stack();
+        // push 
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5);
+        st.push(6);
+
+        // peek
+        System.out.println(st.peek());
     }
 }
