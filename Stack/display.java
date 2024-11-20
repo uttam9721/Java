e1110481da3da02e1b0a3f1f7ef3a780c79e136e
Uttam Kumar <uttammaurya377@gmail.com>
2024-11-20 21:45:27 +0530

    recursion

--- Stack/display.java
+++ Stack/display.java
@@ -3,6 +3,15 @@ import java.util.*;
 
 
 public class display {
+    public static void displayRec(Stack<Integer>st){
+        if (st.size()==0) {
+            return;
+        }
+        int top = st.pop();
+        System.out.print(top+" ");
+        displayRec(st);
+        st.push(top);
+    }
     public static void main(String[] args) {
         Stack<Integer> st = new Stack<>();
         st.push(1);
@@ -10,17 +19,22 @@ public class display {
         st.push(3);
         st.push(4);
         st.push(5);
+        // System.out.print(st);
+        displayRec(st);
+        displayRec(st);
+        // System.out.print(st);
         // System.out.println(st);
-        Stack<Integer> rt = new Stack<>();
-        while (st.size()>0) {
-            rt.push(st.pop());
 
-        }
-        while (rt.size()>0) {
-            int x = rt.pop();
-            System.out.println(x);
-            st.push(x);
-        }
+        // Stack<Integer> rt = new Stack<>();
+        // while (st.size()>0) {
+        //     rt.push(st.pop());
+
+        // }
+        // while (rt.size()>0) {
+        //     int x = rt.pop();
+        //     System.out.println(x);
+        //     st.push(x);
+        // }
         // System.out.println(st);
     }
     
