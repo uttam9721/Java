27c3f9d4e5d36dfffb0243e685e050d29988e124
Uttam Kumar <uttammaurya377@gmail.com>
2024-11-23 23:54:19 +0530

    stack

---
+++ Stack/ReverseStack.java
@@ -0,0 +1,21 @@
+package Stack;
+import java.util.*;
+public class ReverseStack {
+   
+    public static void main(String[] args) {
+        Stack<Integer> s =new Stack<> ();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        // System.out.println(s); 
+        Stack<Integer> rev =new Stack<> ();
+        while (s.size()>0) {
+            rev.push(s.pop());
+            
+        }
+        System.out.println(rev);
+        
+    }
+    
+}
