a51afd8ee0bf9e8b9b969a5dfb8a388e0358600e
Uttam Kumar <uttammaurya377@gmail.com>
2024-11-30 22:57:50 +0530

    practice

---
+++ Practice/HalfNumber.java
@@ -0,0 +1,8 @@
+package Practice;
+
+public class HalfNumber {
+    public static void main(String[] args) {
+        int n=5;
+    }
+    
+}
---
+++ Practice/HalfPyramid.java
@@ -0,0 +1,15 @@
+package Practice;
+
+public class HalfPyramid {
+    public static void main(String[] args) {
+        int n = 4;
+        for(int i = 1; i <=n; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+    }
+    
+}
