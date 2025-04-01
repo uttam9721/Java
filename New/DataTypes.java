7ea517ac854506c84e150bb145fbae5cf6a76155
Uttam Kumar <uttammaurya377@gmail.com>
2025-04-01 21:47:51 +0530

    new

---
+++ New/DataTypes.java
@@ -0,0 +1,20 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+        boolean b = true;
+        int a = 10;
+        short s = 1200;
+        byte by = 120;
+        long l = 1023564;
+        float f = 12.3f;
+        double d = 1235.24;
+        char ch = 'p';
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(by);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(ch);
+    }
+}
--- New/New.java
+++ New/New.java
@@ -1,3 +1,9 @@
 public class New {
-    
+
+    public static void main(String[] args) {
+        int a= 10;
+        a=20;
+        System.out.println(a);
+        System.out.println("Uttam");
+    }
 }
