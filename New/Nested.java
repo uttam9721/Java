5ff352f72111bdbe13cbf2fed11cc0bff89621d4
Uttam Kumar <uttammaurya377@gmail.com>
2025-04-10 23:52:06 +0530

    practice

---
+++ New/Nested.java
@@ -0,0 +1,16 @@
+// import java.util.*;
+
+// class Nested {
+//     public static void main(String[] args) {
+//         for (int i = 1; i <= 3; i++) {
+//             for (int j = 1; j <= 3; j++) {
+//                 System.out.print(i + "," + j + " ");
+//                 // You could also use: System.out.print(i + "+" + j + " ");
+//             }
+//             System.out.println(); // To move to the next line after each inner loop
+//         }
+//     }
+// }
+
+
+// pattern
