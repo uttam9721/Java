96ad9d8174ef7c1ab9b9e9929c34aed58adfff20
Uttam Kumar <uttammaurya377@gmail.com>
2025-04-11 23:48:07 +0530

    done

--- New/Nested.java
+++ New/Nested.java
@@ -14,3 +14,64 @@
 
 
 // pattern
+// package New;
+
+// import java.util.Scanner;
+
+// public class Nested {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int r=sc.nextInt();
+//         int c=sc.nextInt();
+//         for(int i=1;i<=r;i++){
+//             for(int j=1;j<=c;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// ulta star
+// package New;
+
+// import java.util.Scanner;
+
+// public class Nested {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter any number ");
+//         int r = sc.nextInt();
+//         for(int i = 1;i<=r;i++){
+//             for(int j =1;j<=(r+1-i);j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// 
+
+
+package New;
+
+import java.util.Scanner;
+
+public class Nested {
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int r=sc.nextInt();
+        for(int i=1;i<=r;i++){
+            for(int j=1;j<=r-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=2*i-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
