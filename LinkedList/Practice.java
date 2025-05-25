b2b9efca746873f2a8b892bac344c425ae50a905
Uttam Kumar <uttammaurya377@gmail.com>
2025-05-26 01:26:45 +0530

    ll

--- LinkedList/Practice.java
+++ LinkedList/Practice.java
@@ -31,11 +31,22 @@ public class Practice{
         }
         System.out.println("null");
     }
+    void addLast(int data){
+        Node newNode = new Node(data);
+        if (head==null) {
+            head=tail=newNode;
+            return;
+        }
+        tail.next=newNode;
+        tail=newNode;
+    }
 
     public static void main(String[] args) {
         Practice ll = new Practice();
         ll.addFirst(0);
         ll.addFirst(1);
+        ll.addLast(3);
+        ll.addLast(4);
         display();
     }
 }
