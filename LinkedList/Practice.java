88929f4e6bebf060515f0982e9c6d8fd56e7aaf4
Uttam Kumar <uttammaurya377@gmail.com>
2025-05-26 00:38:42 +0530

    ll

---
+++ LinkedList/Practice.java
@@ -0,0 +1,41 @@
+
+package LinkedList;
+
+public class Practice{
+    public static class Node{
+        int data ;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+   void addFirst(int data){
+    Node newNode = new Node(data);
+    if (head==null) {
+        head=tail=newNode;
+        return;
+    }
+    newNode.next=head;
+    head=newNode;
+
+    }
+    static void display(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data+" =>");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Practice ll = new Practice();
+        ll.addFirst(0);
+        ll.addFirst(1);
+        display();
+    }
+}
