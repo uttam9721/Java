6e0f35ca08f5393f7691d02db32f8e220e37c55f
Uttam Kumar <uttammaurya377@gmail.com>
2025-06-09 23:37:47 +0530

    stack

---
+++ Stack/Simple.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+public class Simple {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        System.out.println(st);
+        
+    }
+    
+}
