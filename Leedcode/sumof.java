1128fe06c6767e41e5cfb6d418edc8fa2098fc05
Uttam Kumar <159714121+uttam9721@users.noreply.github.com>
2025-06-11 22:39:59 +0530

    Update sumof.java

--- Leedcode/sumof.java
+++ Leedcode/sumof.java
@@ -14,6 +14,8 @@ public class sumof {
     public static void main(String[] args) {
         int[] nums = {2,7,11,15};
         int target = 9;
+
+        
         System.out.println(twosum(nums, target));
     }
     
