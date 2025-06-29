114dacf3fb5f5f14628a81435981e18f6241bc45
uttam9721 <uttammaurya377@gmail.com>
2025-06-29 16:22:53 +0530

    d

--- Placement/Basic/whileLoop.java
+++ Placement/Basic/whileLoop.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
 public class whileLoop {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter any no of n");
+        System.out.println("Enter any number n:");
         int n = sc.nextInt();
-        while(1>n){
-            System.out.println(n);
-            n++;
+        int i = 1;
+        while (i <= n) {
+            System.out.println(i);
+            i++;
         }
     }
 }
