adfdaad5b9593e5f133a9dd79bd76c6c850bd0bc
uttam9721 <uttammaurya377@gmail.com>
2025-07-01 23:57:59 +0530

    done

---
+++ Placement/Oops/Basic.java
@@ -0,0 +1,8 @@
+public class Basic {
+    int age = 21;
+
+    public static void main(String[] args) {
+        Basic obj = new Basic(); // create object of Basic class
+        System.out.println(obj.age); // access the age variable
+    }
+}
