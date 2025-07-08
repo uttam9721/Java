9edc5a99a6d750973545bba5ba4827495b5e1c7c
uttam9721 <uttammaurya377@gmail.com>
2025-07-09 04:44:04 +0530

    done

--- Placement/Array/Occurance.java
+++ Placement/Array/Occurance.java
@@ -10,6 +10,16 @@ public class Occurance {
         }
         return count;
     }
+    static int lastOccurance(int arr[],int x){
+        int lastIndex =-1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]==x) {
+                lastIndex=i;
+            }
+        }
+        return lastIndex;
+    }
+    
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         System.out.println("Enter the value of An Array");
@@ -20,7 +30,8 @@ public class Occurance {
         for (int i = 0; i < arr.length; i++) {
             arr[i]=sc.nextInt();
         }
-        System.out.println(Repeating(arr, x));
+        // System.out.println(Repeating(arr, x));
+        System.out.println(lastOccurance(arr, x));
     }
     
     
