9253a9d99f41a855f805377745b52b2d5463e32c
uttam9721 <uttammaurya377@gmail.com>
2025-07-09 08:16:05 +0530

    d

--- Placement/Array/Occurance.java
+++ Placement/Array/Occurance.java
@@ -19,19 +19,29 @@ public class Occurance {
         }
         return lastIndex;
     }
+    static boolean isSorted(int arr[]) {
+    for (int i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false; // Not sorted
+        }
+    }
+    return true; // Sorted
+}
+
     
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         System.out.println("Enter the value of An Array");
         int n = sc.nextInt();
-        System.out.println("Enter the value of x");
-        int x =sc.nextInt();
+        // System.out.println("Enter the value of x");
+        // int x =sc.nextInt();
         int []arr=new int[n];
         for (int i = 0; i < arr.length; i++) {
             arr[i]=sc.nextInt();
         }
         // System.out.println(Repeating(arr, x));
-        System.out.println(lastOccurance(arr, x));
+        // System.out.println(lastOccurance(arr, x));
+        System.out.println(isSorted(arr));
     }
     
     
