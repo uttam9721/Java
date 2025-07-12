69860dbf4e0c7793d6f4cc46e50eab37ad84f54c
uttam9721 <uttammaurya377@gmail.com>
2025-07-13 01:12:06 +0530

    d

--- Placement/Oops/StudentClass.java
+++ Placement/Oops/StudentClass.java
@@ -1,24 +1,49 @@
 
+// public class StudentClass {
+//     // creating new dataType.
+//     public static class Student{
+//         String name;   //these are the dataType
+//         int rno;
+//         double percent;
+//     }
+
+//     public static void main(String[] args) {
+//         Student x = new Student();  //this is declaration
+//         x.name = "Uttam";   // these are initialization
+//         x.rno = 24;
+//         x.percent = 92.5;
+//         System.out.println(x.name);
+
+
+//         Student s = new Student();//these are objects
+//         s.name="Maurya";
+//         s.percent=97.2;
+//         s.rno=3;
+//     }
+    
+// }
+
+package Placement.Oops;
+import java.util.*;
+
 public class StudentClass {
-    // creating new dataType.
-    public static class Student{
-        String name;   //these are the dataType
-        int rno;
-        double percent;
+   static class Student{
+        String name;
+        int age;
+        void walk(){
+            System.out.println("Uttam is walking");
+        }
+        void talk(){
+            System.out.println("Uttam is talking");
+        }
     }
 
     public static void main(String[] args) {
-        Student x = new Student();  //this is declaration
-        x.name = "Uttam";   // these are initialization
-        x.rno = 24;
-        x.percent = 92.5;
-        System.out.println(x.name);
-
-
-        Student s = new Student();//these are objects
-        s.name="Maurya";
-        s.percent=97.2;
-        s.rno=3;
+        Student s1=new Student();
+        s1.name="Uttam";
+        s1.age=21;
+        s1.walk();
+        s1.talk();
+        System.out.println(s1.name +" "+s1.age);
     }
-    
 }
---
+++ Placement/Stack/Basic.java
@@ -0,0 +1,8 @@
+package Placement.Stack;
+
+public class Basic {
+    public static void main(String[] args) {
+        System.out.println("Hlo");
+    }
+    
+}
